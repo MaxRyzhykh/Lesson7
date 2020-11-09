62b80efbe25f28d04dceef266c59381620955a28
MaxRyzhykh <72518453+MaxRyzhykh@users.noreply.github.com>
2020-11-09 16:54:34 +0300

    first commit

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,8 +1,32 @@
 package academy.belhard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Person person1 = new Person("Max","Ryzhykh", Gender.MALE);
+        Person person2 = new Person("Max","Ryzhykh", Gender.MALE);
+        Person person3 = new Person("Evgeny","Khrapan", Gender.MALE);
+        Person person4 = new Person("Julia","Edward", Gender.FEMALE);
+        Person person5 = new Person("Eva","Fox", Gender.FEMALE);
+
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+        personList.add(person5);
+
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
+        System.out.println(person4.toString());
+        System.out.println(person5.toString());
+
+        System.out.println(personList.get(0));
     }
 }
---
+++ src/academy/belhard/Person.java
@@ -0,0 +1,69 @@
+package academy.belhard;
+
+public final class Person {
+
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+
+
+    public Person(String firstName, String lastName, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = firstName.hashCode() + 9 * lastName.hashCode() + gender.hashCode();
+        return hash;
+    }
+
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + "   lastName: " + lastName + "   gender: " + gender;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if(!(obj instanceof Person)){
+            return false;
+        }
+
+        Person person = (Person) obj;
+
+        return firstName == person.firstName && lastName == person.lastName && gender == person.gender;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
